31507ccfdb378ae3bcb889dfd88484890d3ccb03
Willy Nzesseu <willynzesseu@gmail.com>
2019-10-23 18:27:29 +0100

    feat(app): create Album and Track interfaces and map data to it

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@
 import {map} from 'rxjs/add/operator/map';
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 //import {map} from 'rxjs/operators';
 
@@ -11,10 +13,10 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
-    .pipe(
-      map(response => response.json())
-      );
+    .map(
+      response => <Album>response.json()
+    );
   }
 }
