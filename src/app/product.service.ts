0c3200765b617e4d76c37df78d64d30807078a1f
Willy Nzesseu <willynzesseu@gmail.com>
2019-10-24 09:20:28 +0100

    feat(app): add product list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,12 +4,14 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 // import {map} from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -17,6 +19,13 @@ export class ProductService {
     return this._http.get(this._albumUrl)
     .map(
       response => <Album>response.json()
+    );
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl)
+    .map(
+      response => <Product[]>response.json()
     );
   }
 }
